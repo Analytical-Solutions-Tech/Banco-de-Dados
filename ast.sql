d72da48240aa67b0aef66a17926955be0dbb1d3c
PedroPradoCho <pedro.araujo@sptech.school>
2023-03-31 23:18:13 -0300

    alterações e anotações nas tabelas

--- ast.sql
+++ ast.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE ast;
 USE ast;
 -- ver se precisa da tabela produto
--- Criar uma tabela usuario
 CREATE TABLE cliente(
     idCliente INT PRIMARY KEY AUTO_INCREMENT,
     Cnpj char(14) NOT NULL,
     Nome_fantasia VARCHAR(100) NOT NULL,
-    Contato VARCHAR(50),
-    Email VARCHAR(255) NOT NULL,
-    Senha VARCHAR(255) NOT NULL
+    Contato VARCHAR(50), -- char limitndo numero de celular
+    Email VARCHAR(255) NOT NULL, 
+    Senha VARCHAR(255) NOT NULL -- char senha
 );
 
 INSERT INTO cliente VALUES
@@ -20,6 +19,22 @@ INSERT INTO cliente VALUES
 (DEFAULT, '19773504000172', 'Fish Bem','(96) 2342-4576','fish.bem@gmail.com','6D^z121qIWOp'),
 (DEFAULT, '19773504000172', 'New Fish','(63) 3525-4692','new.fish@gmail.com','6D^z121qIWOp');
 SELECT * FROM cliente;
+
+CREATE TABLE usuario( -- fkempresa
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    Cpf char (11) NOT NULL,
+    Nome varchar(100),
+    Contato varchar(50), -- char limitndo numero de celular
+    Email varchar(100),
+    Senha varchar(100)); -- char senha
+
+INSERT INTO usuario VALUES -- add fks
+(null,'00000000000','Pedro','199999999','pedro@newfish','12345'),
+(null,'11111111111','Inacio','1188888888','inacio@fishbem','12345'),
+(null,'22222222222','Diogo','1177777777','diogo@fishnavio','12345'),
+(null,'33333333333','Amanda','1166666666','amanda@wantfish','12345'),
+(null,'44444444444','Abraao','1155555555','abraao@fisheat','12345'),
+(null,'55555555555','Lucas','11444444444','lucas@lovefish','12345');
 
 CREATE TABLE sensor(
     numero_serie INT PRIMARY KEY NOT NULL,
