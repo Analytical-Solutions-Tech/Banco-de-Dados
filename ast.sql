ff7e3ce1e92fd9dfd216ee656bfcf28e96ec37cf
Diogo Vidal <111745761+diogovidall@users.noreply.github.com>
2023-04-04 08:06:58 -0300

    Update Scripts
    Adicionei as Foreign Keys e organizei o banco de dados em geral.

--- ast.sql
+++ ast.sql
@@ -1,74 +1,82 @@
 CREATE DATABASE ast;
 USE ast;
--- ver se precisa da tabela produto
-CREATE TABLE cliente(
-    idCliente INT PRIMARY KEY AUTO_INCREMENT,
-    Cnpj char(14) NOT NULL,
-    Nome_fantasia VARCHAR(100) NOT NULL,
-    Contato VARCHAR(50), -- char limitndo numero de celular
-    Email VARCHAR(255) NOT NULL, 
-    Senha VARCHAR(255) NOT NULL -- char senha
+
+-- Tabela onde cadastramos os dados de nossos clientes
+Create Table clienteEmpresa(
+    idCliente Int Primary Key Auto_increment,
+    Cnpj Char(14) Not Null,
+    Nome_fantasia Varchar(25) Not Null,
+    Contato Char(15) Not Null,
+    Email Varchar(255) Not Null, 
+    Senha Char(8) Not Null 
 );
 
-INSERT INTO cliente VALUES
-(DEFAULT, '19773504000172', 'Headline Fish', '(61) 3452-2863', 'headline.fish@gmail.com', '6Ll3h9*JZ6m6'),
-(DEFAULT, '19773504000172','Fish Eat', '(61) 2784-4764', 'fish.eat@gmil.com', 'U!mA4DpG22%i'),
-(DEFAULT, '19773504000172', 'Love Fish', '(67) 2549-8845', 'love.fish@gmail.com','Oe3s21r1*jx*'),
-(DEFAULT, '19773504000172', 'Want Fish', '(11) 3434-7103','want.fish@gmail.com','jT999!hXTSOD'),
-(DEFAULT, '19773504000172', 'Fish Navio','(69) 2616-5122', 'fish.navio@gmail.com', '8oC7u8%u4A1^'),
-(DEFAULT, '19773504000172', 'Fish Bem','(96) 2342-4576','fish.bem@gmail.com','6D^z121qIWOp'),
-(DEFAULT, '19773504000172', 'New Fish','(63) 3525-4692','new.fish@gmail.com','6D^z121qIWOp');
-SELECT * FROM cliente;
+Insert Into clienteEmpresa Values
+(Null, '19773504000172', 'Headline Fish', '(61) 3452-2863', 'headline.fish@gmail.com', '6Ll3h9*J'),
+(Null, '19773504000172','Fish Eat', '(61) 2784-4764', 'fish.eat@gmil.com', 'U!mA4DpG'),
+(Null, '19773504000172', 'Love Fish', '(67) 2549-8845', 'love.fish@gmail.com','Oe3s21r1'),
+(Null, '19773504000172', 'Want Fish', '(11) 3434-7103','want.fish@gmail.com','jT999!hX'),
+(Null, '19773504000172', 'Fish Navio','(69) 2616-5122', 'fish.navio@gmail.com', '8oC7u8%'),
+(Null, '19773504000172', 'Fish Bem','(96) 2342-4576','fish.bem@gmail.com','6D^z121'),
+(Null, '19773504000172', 'New Fish','(63) 3525-4692','new.fish@gmail.com','6D^z1293');
+SELECT * FROM Cliente;
 
-CREATE TABLE usuario( -- fkempresa
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    Cpf char (11) NOT NULL,
-    Nome varchar(100),
-    Contato varchar(50), -- char limitndo numero de celular
-    Email varchar(100),
-    Senha varchar(100)); -- char senha
-
-INSERT INTO usuario VALUES -- add fks
-(null,'00000000000','Pedro','199999999','pedro@newfish','12345'),
-(null,'11111111111','Inacio','1188888888','inacio@fishbem','12345'),
-(null,'22222222222','Diogo','1177777777','diogo@fishnavio','12345'),
-(null,'33333333333','Amanda','1166666666','amanda@wantfish','12345'),
-(null,'44444444444','Abraao','1155555555','abraao@fisheat','12345'),
-(null,'55555555555','Lucas','11444444444','lucas@lovefish','12345');
-
-CREATE TABLE sensor(
-    numero_serie INT PRIMARY KEY NOT NULL,
-    tipo VARCHAR(255),
-    modelo VARCHAR(255),
-     valor DOUBLE
+-- Tabela onde a empresa cadastra os dados de seus colaboradores
+Create Table Usuario( 
+	idUsuario Int Primary Key Auto_increment,
+    Cpf Char(11) Not Null ,
+    Nome Varchar(100) Not Null,
+    Contato Char(15) Not Null,
+    Email Varchar(255) Not Null,
+    Senha Char(8) Not Null,
+    fkEmpresa Int,
+    Foreign Key (fkEmpresa) References clienteEmpresa(idCliente)
 );
-ALTER TABLE sensor ADD COLUMN statusSensor VARCHAR(7); 
-ALTER TABLE sensor ADD CONSTRAINT chk_statusSensor CHECK(statusSensor IN('Ativo', 'Inativo'));
--- constraint chk_statusSensor já criada, aceitando apenas valores ativo ou inativo na coluna statusSensor
 
-INSERT INTO sensor VALUES
-(12022003, 'temperatura','LM35', 20),
-(07042019, 'umidade e temperatura','DHT11', 15),
-(08032004, 'luminosidade','LDR5', 23),
-(05032009, 'bloqueio','TCRT5000', 10);
+Insert Into Usuario Values 
+(Null,'08517408985','Pedro','(93)998644378','pedro@newfish','128394', 1),
+(Null,'77468787506','Inacio','(95)995525657','inacio@fishbem','86245', 3),
+(Null,'84942442800','Diogo','(46)999785779','diogo@fishnavio','12746', 2),
+(Null,'88283094580','Amanda','(51)968928134','amanda@wantfish','92345', 4),
+(Null,'52783362503','Abraao','(84)988421486','abraao@fisheat','52345', 5),
+(Null,'75478786365','Lucas','(22)967167494','lucas@lovefish','84365', 1);
+SELECT * FROM Usuario;
+SELECT * FROM Usuario Join clienteEmpresa on fkEmpresa = idCliente;
+
+
+-- Tabela onde registramos o sensor em si. Seja o tipo dele, seu valor e seu andamento.
+Create Table Sensor(
+    numero_serie Int Primary Key Auto_increment,
+    tipo_sensor Varchar(25) Not Null,
+    modelo_sensor Varchar(20) Not Null,
+	valor_sensor Varchar(25) Not Null,
+    status_sensor Varchar(8) Check(status_sensor in('Ativo', 'Inativo')) Not Null
+);
+
+Insert Into Sensor Values
+(12022003, 'Temperatura','LM35','R$20', 'Ativo'),
+(07042019, 'Umidade e temperatura','DHT11','R$15', 'Inativo'),
+(08032004, 'luminosidade','LDR5', 'R$23' , 'Inativo'),
+(05032009, 'Bloqueio','TCRT5000', 'R$10' , 'Inativo');
 SELECT * FROM sensor;
 
 
--- Colocar check se o transporte está parado ou em movimento, ou em manuntenção
-CREATE TABLE registro(
-    idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-    temperatura DOUBLE,
-    transporte varchar(45),
-    data_hora DATETIME
-);
+-- Nessa tabela é onde fica os registros dos sensores juntamente com a situação do transporte.
+Create Table leituraDiaria(
+    idLeitura Int Primary Key Auto_increment,
+    temperatura_sensor Double Not Null,
+    status_transporte Varchar(40) Check(status_transporte in('Transporte parado', 'Em trânsito', 'Em manuntenção')) Not Null,
+    data_hora Datetime Not Null,
+    fkSensor Int,
+    Foreign Key (fkSensor) References Sensor(numero_serie)
+)auto_increment = 100;
 
-INSERT INTO registro VALUES
-(DEFAULT, 22.0, null,'2022-12-01 00:00:00'),
-(DEFAULT, 22.0, null,'2022-12-01 00:05:00'),
-(DEFAULT, 21.0, null,'2022-12-01 00:10:00'),
-(DEFAULT, 21.5, null,'2022-12-01 00:15:00'),
-(DEFAULT, 22.5, null,'2022-12-01 00:20:00'),
-(DEFAULT, 21.0, null,'2022-12-01 00:25:00');
-SELECT * FROM registro;
--- Criar tabela pedido e colocar a localização, tipo_transporte
--- que vai ser relacionada com a tabela registro e empresa
+Insert Into leituraDiaria Values
+(Null, 22.0, 'Transporte Parado' ,'2022-12-01 00:00:00', 12022003),
+(Null, 22.0, 'Em trânsito','2022-12-01 00:05:00', 12022003),
+(Null, 21.0, 'Em manuntenção' ,'2022-12-01 00:10:00', 12022003),
+(Null, 21.5, 'Transporte parado' ,'2022-12-01 00:15:00', 12022003),
+(Null, 22.5, 'Em manuntenção' ,'2022-12-01 00:20:00', 12022003),
+(NUll, 21.0, 'Em trânsito' ,'2022-12-01 00:25:00', 12022003);
+SELECT * FROM leituraDiaria;
+SELECT* FROM Sensor Join leituraDiaria on fkSensor = numero_serie;
